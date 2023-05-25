b3fce2d4728155d30557a93510137055436e5ef7
beezital <63583702+beezital@users.noreply.github.com>
2023-05-25 14:21:51 +0200

    Initial Commit

---
+++ navigationSandpit/navigationSandpitApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationSandpitApp.swift
+//  navigationSandpit
+//
+//  Created by Fred on 25.05.23.
+//
+
+import SwiftUI
+
+@main
+struct navigationSandpitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
